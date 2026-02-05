fdc345f2d5669c8439eac44f03b665752721ec32
Nikodem <nikodemmlynski08@gmail.com>
2026-02-05 23:54:18 +0100

    fixed db session

--- app/database/session.py
+++ app/database/session.py
@@ -6,6 +6,11 @@ from app.core.config import settings
 engine = create_async_engine(settings.DATABASE_URL)
 SessionLocal = async_sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession, expire_on_commit=False)
 
+# Sync Session for Celery/Tools
+SQLALCHEMY_DATABASE_URL_SYNC = settings.DATABASE_URL.replace("+asyncpg", "")
+sync_engine = create_engine(SQLALCHEMY_DATABASE_URL_SYNC)
+SyncSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=sync_engine)
+
 async def get_db():
     async with SessionLocal() as session:
         yield session
